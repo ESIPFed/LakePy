bee96f5338df63af473a1577ddd8ae1297a2fbe8
jakegearon <uFZ!_g68msGpyWXoEzot>
2020-12-21 17:16:07 -0500

    incremented version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.0.9'
+VERSION = '1.0.10'
 PACKAGE_NAME = 'lakepy'
 AUTHOR = 'James H. Gearon & John D. Franey'
 AUTHOR_EMAIL = 'jake.gearon@gmail.com'
